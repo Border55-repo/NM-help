8217c5dea2f1e7637ec788456e66715e875704d3
Border55-repo <juliannordli@gmail.com>
2026-09-05 16:18:54 +0200

    Rename dist/service-worker.js to NM Help

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "nordic-borsvakt-v1";
+const CACHE = "nm-help-v1";
 const FILES = ["./", "index.html", "styles.css", "app.js", "analyzer.js", "manifest.webmanifest", "icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
